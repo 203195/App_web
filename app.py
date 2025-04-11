5c6ecb03d41ae04383e8093b681caf9039dc44c8
203195 <203195@iberopuebla.mx>
2025-04-11 12:07:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 
 app = Flask(__name__)
 
+catalogo = [
+    {'id': 1, 'nombre': 'Pan', 'precio': 20},
+    {'id': 2, 'nombre': 'Leche', 'precio': 10},
+    {'id': 3, 'nombre': 'Huevo', 'precio': 5},
+    {'id': 4, 'nombre': 'Queso', 'precio': 30},
+    {'id': 5, 'nombre': 'Carne', 'precio': 80},
+]
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    print(catalogo)
+    return render_template('index.html', productos=catalogo)
+
+@app.route('/agregar/<int:id>')
+def agregar(id):
+    if 'carrito' not in session:
+        session['carrito'] = []
+    session['carrito'].append(id)
+    session.modified = True
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
